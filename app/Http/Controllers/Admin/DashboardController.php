2da4368b6c7a9a48b5aa84f059e95020f7879836
alifalfasa <uchihaalfasa261@gmail.com>
2024-12-18 14:58:51 +0700

    Update Dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -13,16 +13,12 @@ class DashboardController extends Controller
     public function DashboardIndex()    
     {
         $memberCount = Users::count();
-<<<<<<< Updated upstream
-
-        return view('admin.pages.dashboard.index',compact('memberCount')); // Sesuaikan dengan view yang ada
-=======
         $platinumCount = Users::where('kode_paket','like', 'Platinum%')->count();
         $goldCount = Users::where('kode_paket','like', 'Gold%')->count();
         $freeCount = Users::where('kode_paket','like', 'Free%')->count();
 
+        // dd($memberCount);
         return view('admin.pages.dashboard.index',compact('memberCount', 'platinumCount', 'goldCount', 'freeCount')); // Sesuaikan dengan view yang ada
->>>>>>> Stashed changes
     }
     
 }
